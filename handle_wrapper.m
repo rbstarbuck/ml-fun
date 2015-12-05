feb03dd48566baa2b7b54c234ffaeaef24597f8d
Yixing <liuyixing10011@gmail.com>
2015-12-05 17:24:03 -0500

    Update handle_wrapper.m

--- handle_wrapper.m
+++ handle_wrapper.m
@@ -17,7 +17,8 @@ function features = handle_wrapper(input_matrix)
  f3 = @var;
  weight3 = 1;
  f4 = @(x) median(abs(x));
- weight4 = 1;
+ weight4 = 0.7;
+ f4 = @pca_handle;
  
- features = [weight1*f1(input_matrix), weight2*f2(input_matrix), weight3*f3(input_matrix)];%, weight4*f4(input_matrix)];
+ features = [weight1*f1(input_matrix), weight2*f2(input_matrix), weight3*f3(input_matrix), weight4*f4(input_matrix)];
 end
